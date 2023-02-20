8eea4d1ed60a2dd6b60a9461b409eaa0f13bc75a
Iclementem <iclementem@outlook.es>
2023-02-20 07:33:16 +0100

    Initial Commit

---
+++ SwiftUI_TabView/SwiftUI_TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TabViewApp.swift
+//  SwiftUI_TabView
+//
+//  Created by Iván Clemente Moreno on 20/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
